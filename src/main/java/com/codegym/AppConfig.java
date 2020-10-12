1859aa4cc11a75d0231b546692a95d81c0804ef9
Tên bạnlucle <lucle99994@gmail.com>
2020-10-12 15:36:31 +0700

    first commit

--- src/main/java/com/codegym/AppConfig.java
+++ src/main/java/com/codegym/AppConfig.java
@@ -30,77 +30,80 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
-@Configuration
-@EnableWebMvc
-@ComponentScan("com.codegym")
-public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
-    private ApplicationContext appContext;
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan("com.codegym")
+    public class AppConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+        private ApplicationContext appContext;
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        appContext = applicationContext;
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            appContext = applicationContext;
+        }
+
+        @Bean
+        public ViewResolver viewResolver() {
+            ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+            viewResolver.setTemplateEngine(templateEngine());
+            return viewResolver;
+        }
+
+        @Bean
+        public TemplateEngine templateEngine() {
+            TemplateEngine templateEngine = new SpringTemplateEngine();
+            templateEngine.setTemplateResolver(templateResolver());
+            return templateEngine;
+        }
+
+        @Bean
+        public ITemplateResolver templateResolver() {
+            SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+            templateResolver.setApplicationContext(appContext);
+            templateResolver.setPrefix("/WEB-INF/templates/");
+            templateResolver.setSuffix(".html");
+            templateResolver.setTemplateMode(TemplateMode.HTML);
+            return templateResolver;
+        }
+
+        @Bean
+        public CustomerService customerService() {
+            return new SimpleCustomerServiceImpl();
+        }
+
+        @Bean
+        public DataSource dataSource() {
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://localhost:3306/sms");
+            dataSource.setUsername("root");
+            dataSource.setPassword("04032001");
+            return dataSource;
+        }
+
+        Properties additionalProperties() {
+            Properties properties = new Properties();
+            properties.setProperty("Hibernate.hbm2ddl.auto", "update");
+            properties.setProperty("Hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+            return properties;
+        }
+
+        @Bean
+        @Qualifier(value = "entityManager")
+        public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
+            return entityManagerFactory.createEntityManager();
+        }
+
+        @Bean
+        public FactoryBean<EntityManagerFactory> entityManagerFactory() {
+            LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+            em.setDataSource(dataSource());
+            em.setPackagesToScan("com.codegym.model");
+
+            JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+            em.setJpaVendorAdapter(vendorAdapter);
+
+            em.setJpaProperties(additionalProperties());
+            return em;
+        }
     }
 
-    @Bean
-    public ViewResolver viewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(templateEngine());
-        return viewResolver;
-    }
-
-    @Bean
-    public TemplateEngine templateEngine() {
-        TemplateEngine templateEngine = new SpringTemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver());
-        return templateEngine;
-    }
-
-    @Bean
-    public ITemplateResolver templateResolver() {
-        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
-        templateResolver.setApplicationContext(appContext);
-        templateResolver.setPrefix("/WEB-INF/templates/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode(TemplateMode.HTML);
-        return templateResolver;
-    }
-
-    @Bean
-    public CustomerService customerService() {
-        return new SimpleCustomerServiceImpl();
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/sms");
-        dataSource.setUsername("root");
-        dataSource.setPassword("04032001");
-        return dataSource;
-    }
-
-    Properties additionalProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("Hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("Hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        return properties;
-    }
-    @Bean
-    @Qualifier(value = "entityManager")
-    public EntityManager entityManager(EntityManagerFactory entityManagerFactory){
-        return  entityManagerFactory.createEntityManager();
-    }
-    @Bean
-    public FactoryBean<EntityManagerFactory> entityManagerFactory() {
-        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
-        em.setPackagesToScan("com.codegym.model");
-
-        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        em.setJpaVendorAdapter(vendorAdapter);
-
-        em.setJpaProperties(additionalProperties());
-        return em;
-    }
-}
--- src/main/java/com/codegym/model/Customer.java
+++ src/main/java/com/codegym/model/Customer.java
@@ -1,6 +1,12 @@
 package com.codegym.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table
 public class Customer implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
